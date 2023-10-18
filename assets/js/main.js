a32bda6923411c86a17fdf55985a6ef0eb822d63
Burhanverse <burhanverse@gmail.com>
2023-10-19 02:05:30 +0530

    RUI: Formated source codes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,3 @@
-/**
-* Template Name: OnePage
-* Updated: Mar 10 2023 with Bootstrap v5.2.3
-* Template URL: https://bootstrapmade.com/onepage-multipurpose-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-
 function toggleDarkMode() {
   document.body.classList.toggle('dark-mode');
 }
